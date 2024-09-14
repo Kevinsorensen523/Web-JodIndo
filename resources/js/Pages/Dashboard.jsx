91be9324c9f4cf63eeb6980e02695b32369ad1e5
kevinsorensen523 <kevinsorensen523@gmail.com>
2024-09-14 12:39:50 +0700

    Initial Commit

---
+++ resources/js/Pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Dashboard() {
+    return (
+        <div className="bg-blue-500 p-10">
+            <h1 className="text-2xl font-bold text-black">
+                Welcome to the Web-JodIndo Dashboard
+            </h1>
+            <p className="text-white">
+                This is your dashboard, using Inertia.js with React and Tailwind
+                CSS.
+            </p>
+        </div>
+    );
+}
